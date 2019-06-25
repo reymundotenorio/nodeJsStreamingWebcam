c1db648254ce5fce1e13e95b079f90e3726b464d
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-24 22:23:50 -0700

    Server with socket.io

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+
 const path = require("path");
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const socket = require("socket.io")(server);
+const io = require("socket.io")(server);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
   console.log("Listening port 3000...");
 });
+
+setInterval(() => {
+  io.emit("image", "sending data");
+}, 1000);
 
 server.listen(3000);
