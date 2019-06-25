e67ec1c0348b04b186b45a3663b89bf73fda5e73
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-24 22:10:43 -0700

    Adding server with http

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const socket = require("socket.io")(server);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  console.log("Listening port 3000...");
 });
 
-app.listen(3000);
+server.listen(3000);
