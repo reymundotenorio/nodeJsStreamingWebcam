679b474daa001a3f425a51193f19a7fd612747f2
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-28 06:26:43 -0700

    Adding working video and audio capture

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-const vCap = new cv.VideoCapture(0);
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
   console.log("Listening port 3000...");
 });
 
-setInterval(() => {
-  io.emit("image", "Hello");
-}, 1000);
+// setInterval(() => {
+//   io.emit("image", "Hello");
+// }, 1000);
 
 server.listen(3000);
