29c39a69d1e32aefc32617ba9b3d13d7dbe7b8b4
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-28 07:51:37 -0700

    Creating logger

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const router = app.Router();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-const Log = require("log"),
-  log = new Log("debug");
+const log = require("log");
 const port = process.env.port || 3000;
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-router.get("/visitors", (req, res) => {
-  res.sendFile(path.join(__dirname, "visitors.html"));
+app.get("/visitors", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/visitors.html"));
 });
-
-// Add the router
-app.use("/", router);
 
 // Listening new client connection
 io.on("connection", socket => {
@@ -30,5 +25,5 @@ io.on("connection", socket => {
 
 // Running server
 server.listen(port, () => {
-  log.info(`Server listening por ${port}`);
+  log("Server listening on port %s", port);
 });
