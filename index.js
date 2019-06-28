098a5386aca67eac1fca8dcea9333b82a1ef2aad
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-28 06:54:10 -0700

    Adding log and router

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const path = require("path");
 const express = require("express");
 const app = express();
+const path = require("path");
+const router = app.Router();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const Log = require("log");
 
-app.get("/", (req, res) => {
+const log = new Log("debug");
+
+const port = process.env.port || 3000;
+
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-  console.log("Listening port 3000...");
 });
+
+router.get("/visitors", (req, res) => {
+  res.sendFile(path.join(__dirname, "visitors.html"));
+});
+
+// Add the router
+app.use("/", router);
 
 // setInterval(() => {
 //   io.emit("image", "Hello");
 // }, 1000);
 
-server.listen(3000);
+// Running server
+server.listen(port, () => {
+  log.info(`Server listening por ${port}`);
+});
